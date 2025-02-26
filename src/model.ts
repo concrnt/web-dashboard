e0135ad49cb91017bbdf3ab779a2d6df406f8fc0
totegamma <github@gammalab.net>
2025-02-26 18:48:14 +0900

    init

---
+++ src/model.ts
@@ -0,0 +1,15 @@
+
+export interface DomainProfile {
+  nickname: string
+  description: string
+  logo: string
+  wordmark: string
+  themeColor: string
+  maintainerName: string
+  maintainerURL: string
+  registration: string
+  version: string
+  hash: string
+  captchaSiteKey: string
+}
+
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,8 @@
+
+export const Welcome = (): JSX.Element => {
+
+    return (
+        <>You might looking for <a href="https://concrnt.world/">Concrnt World</a></>
+    )
+
+}
